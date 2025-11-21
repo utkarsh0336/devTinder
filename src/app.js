528e03c03fbb9b18536aace65122b3d1846a45ae
utkarsh0336 <110279358+utkarsh0336@users.noreply.github.com>
2025-11-21 20:29:08 +0530

    exploring get , post , delete

--- src/app.js
+++ src/app.js
@@ -2,7 +2,17 @@ const express = require("express");
 
 const app = express();   // instance of express js application
 
-// Handling a request
+/**
+ * When I use app.use() , the order of code matters it check for the exactly match in the order and prints the response immediately
+ * app.use() match all the HTTP methods to the route passed.
+ * So to avoid this we use app.get()
+ * but app.get() only handle the GET call to the route passed
+ */
+
+// app.use("/", (req,res) => {
+//     res.send("HAHAHAHA");
+// });   This code will give HAHAHAHA always because the order matters and we are using use().
+
 app.get("/",(req,res) => {  // This is known as Request Handler
     res.send("Hello from the Home !");
 });
@@ -14,6 +24,21 @@ app.get("/hello",(req,res) => {  // This is known as Request Handler
 app.get("/test",(req,res) => {  // This is known as Request Handler
     res.send("Hello from the server !");
 });
+
+app.get("/user",(req,res) => {  // This is known as Request Handler
+    res.send({firstName: "Utkarsh", lastName: "Sahay"});
+});
+
+app.post("/user",(req,res) => {  // This is known as Request Handler
+    // saving data to db
+    res.send("Data successfully saved to the database");
+});
+
+app.delete("/user",(req,res) => {  // This is known as Request Handler
+    res.send("Deleted Successfully");
+});
+
+
 
 app.listen(3000, () => {
     console.log("Server is successfully listening on port 3000...")
